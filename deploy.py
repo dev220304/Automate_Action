f6ee0f967a18c276dce91465fcf6ded98caa34f4
dev220304 <150342009+dev220304@users.noreply.github.com>
2026-04-20 11:24:16 +0530

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,7 +1,12 @@
-
-
 import boto3
+import os
 
 s3 = boto3.client('s3')
 
-s3.upload_file('index.html', 'github-automoted-action-dev-22', 'index.html')
+BUCKET_NAME = 'github-automoted-action-dev-22'
+
+# Upload all image files from repo
+for file in os.listdir():
+    if file.endswith(('.jpg', '.jpeg', '.png')):
+        print(f"Uploading {file}...")
+        s3.upload_file(file, BUCKET_NAME, file)
